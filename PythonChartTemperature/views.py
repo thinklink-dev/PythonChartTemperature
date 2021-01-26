ffbebc5c119c993cf03d1e9c8a9af226de5dd62a
Think Link <software@thinklink.com.co>
2021-01-26 10:52:37 -0500

    Grafica Temperatura y librerias pendientes

--- PythonChartTemperature/views.py
+++ PythonChartTemperature/views.py
@@ -3,8 +3,9 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from PythonChartTemperature import app
+
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +36,42 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/chart')
+def chart():
+    from bokeh.plotting import figure, output_file, show, ColumnDataSource
+    from bokeh.embed import components
+    import pandas
+
+    # Read CSV File Temperature data
+    df = pandas.read_csv('PythonChartTemperature/temperature.csv')
+
+    # Create DATASOURCE from data
+    source = ColumnDataSource(df)
+
+    # List
+    box_list = source.data['Cajon'].tolist()
+
+    p = figure(
+        y_range = box_list,
+        plot_width = 800,
+        plot_height = 600,
+        title = 'Temperatura de Cajones',
+        x_axis_label = 'Temperatura',
+        tools = "pan, box_select, zoom_in, zoom_out, save, reset"        
+    )
+
+    p.hbar(
+        y = 'Cajon',
+        right = 'Temperatura',
+        left = 0,
+        height = 0.4,
+        color = 'orange',
+        fill_alpha = 0.5,
+        source = source
+    )
+    
+    script, div = components(p)
+
+    return render_template('chart.html', script=script, div=div)
+
---
+++ env/Scripts/bokeh-script.py
@@ -0,0 +1,12 @@
+#!C:\DEV\think-link\dev_2021\hallbar_kakao\PythonChartTemperature\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'bokeh==2.2.3','console_scripts','bokeh'
+__requires__ = 'bokeh==2.2.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('bokeh==2.2.3', 'console_scripts', 'bokeh')()
+    )
